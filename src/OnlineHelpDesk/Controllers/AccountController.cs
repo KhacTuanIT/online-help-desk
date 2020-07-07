573ecb290f1a56101a147a058852545d6b12a1c1
HieuVV <voxvanhieu@gmail.com>
2020-07-07 19:28:59 +0700

    Use FindByEmailAsync method

--- src/OnlineHelpDesk/Controllers/AccountController.cs
+++ src/OnlineHelpDesk/Controllers/AccountController.cs
@@ -73,11 +73,12 @@ namespace OnlineHelpDesk.Controllers
                 return View(model);
             }
 
-            var username = UserManager.FindByEmail(model.Email).UserName;
+            // Get user by email
+            var user = await UserManager.FindByEmailAsync(model.Email);
 
             // This doesn't count login failures towards account lockout
             // To enable password failures to trigger account lockout, change to shouldLockout: true
-            var result = await SignInManager.PasswordSignInAsync(username, model.Password, model.RememberMe, shouldLockout: false);
+            var result = await SignInManager.PasswordSignInAsync(user.UserName, model.Password, model.RememberMe, shouldLockout: false);
             switch (result)
             {
                 case SignInStatus.Success:
