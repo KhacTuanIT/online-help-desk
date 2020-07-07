efa873c9ffff78918e86bfd1b7e1b41de3b3728f
SinJunior <khtuankendy@gmail.com>
2020-07-07 17:52:09 +0700

    Modified Views - LoginView - ChangePasswordView - Home/IndexView - Add Action ChangePassword AccountController

--- src/OnlineHelpDesk/Controllers/AccountController.cs
+++ src/OnlineHelpDesk/Controllers/AccountController.cs
@@ -403,6 +403,12 @@ namespace OnlineHelpDesk.Controllers
             return View();
         }
 
+        [AllowAnonymous]
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
