a58d54eff439a2238a6b95e3832dd2cf2fa999e6
SinJunior <khtuankendy@gmail.com>
2020-07-11 19:51:01 +0700

    Initialize data sample for: - Facility - StatusType - RequestType - EquipmentType - Equipment

--- src/OnlineHelpDesk/Controllers/HomeController.cs
+++ src/OnlineHelpDesk/Controllers/HomeController.cs
@@ -32,5 +32,10 @@ namespace OnlineHelpDesk.Controllers
         {
             return View();
         }
+
+        public ActionResult CreateNewRequest()
+        {
+            return View();
+        }
     }
 }
