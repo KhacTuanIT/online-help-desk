a51928b3646cdd99e3dc97757910a1dcdfed14fd
SinJunior <khtuankendy@gmail.com>
2020-07-13 14:51:25 +0700

    Divide Layout to Partial - Update Bundles - Update css and js file

--- src/OnlineHelpDesk/App_Start/BundleConfig.cs
+++ src/OnlineHelpDesk/App_Start/BundleConfig.cs
@@ -9,31 +9,28 @@ namespace OnlineHelpDesk
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/AppJavascript/jquery/jquery.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/AppJavaScript/jquery-validation/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                        "~/Scripts/AppJavaScript/moderniz/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/AppJavaScript/bootstrap/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css",
-                      "~/Content/font-awesome.css",
-                      "~/Content/datatables.min.css"));
+                      "~/Content/AppStyleSheet/bootstrap/bootstrap.css",
+                      "~/Content/AppStyleSheet/css/font-awesome.css",
+                      "~/Content/AppStyleSheet/datatables/datatables.min.css"));
 
             bundles.Add(new StyleBundle("~/Content/login").Include(
-                      "~/Content/login.css"));
-            bundles.Add(new ScriptBundle("~/bundles/custom").Include(
-                      "~/Scripts/index_custom.js"));
+                      "~/Content/AppStyleSheet/css/login.css"));
             bundles.Add(new ScriptBundle("~/bundles/datatables").Include(
-                      "~/Scripts/datatables.min.js"));
+                      "~/Scripts/AppJavaScript/datatables/datatables.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/stylesheet").Include(
                       "~/Content/AppStyleSheet/bootstrap/bootstrap.min.css",
--- src/OnlineHelpDesk/Scripts/AppJavaScript/js/charts-home.js
+++ src/OnlineHelpDesk/Scripts/AppJavaScript/js/charts-home.js
@@ -59,243 +59,243 @@ $(function () {
     });
 
 
-    // ------------------------------------------------------- //
-    // Line Chart
-    // ------------------------------------------------------ //
+    //// ------------------------------------------------------- //
+    //// Line Chart
+    //// ------------------------------------------------------ //
 
-    var LINECHART = $('#lineChart2');
-    var myLineChart = new Chart(LINECHART, {
-        type: 'line',
-        options: {
-            scales: {
-                xAxes: [{
-                    display: false
-                }],
-                yAxes: [{
-                    ticks: {
-                        max: 50,
-                        min: 0
-                    },
-                    display: false
-                }]
-            },
-            legend: {
-                display: false
-            }
-        },
-        data: {
-            labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"],
-            datasets: [{
-                label: "Page Visitors",
-                fill: true,
-                lineTension: 0.4,
-                backgroundColor: "transparent",
-                borderColor: blue,
-                pointBorderColor: blue,
-                pointHoverBackgroundColor: blue,
-                borderCapStyle: 'butt',
-                borderDash: [],
-                borderDashOffset: 0.0,
-                borderJoinStyle: 'miter',
-                borderWidth: 3,
-                pointBackgroundColor: "#fff",
-                pointBorderWidth: 5,
-                pointHoverRadius: 5,
-                pointHoverBorderColor: "#fff",
-                pointHoverBorderWidth: 1,
-                pointRadius: 0,
-                pointHitRadius: 1,
-                data: [20, 14, 21, 15, 22, 8, 18, 13, 21, 13, 17, 13, 20, 15],
-                spanGaps: false
-            }]
-        }
-    });
-
-
-    // ------------------------------------------------------- //
-    // Line Chart 3
-    // ------------------------------------------------------ //
-
-    var LINECHART = $('#lineChart3');
-    var myLineChart = new Chart(LINECHART, {
-        type: 'line',
-        options: {
-            scales: {
-                xAxes: [{
-                    display: false
-                }],
-                yAxes: [{
-                    ticks: {
-                        max: 50,
-                        min: 0
-                    },
-                    display: false
-                }]
-            },
-            legend: {
-                display: false
-            }
-        },
-        data: {
-            labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"],
-            datasets: [{
-                label: "Page Visitors",
-                fill: true,
-                lineTension: 0.4,
-                backgroundColor: "transparent",
-                borderColor: red,
-                pointBorderColor: red,
-                pointHoverBackgroundColor: red,
-                borderCapStyle: 'butt',
-                borderDash: [],
-                borderDashOffset: 0.0,
-                borderJoinStyle: 'miter',
-                borderWidth: 3,
-                pointBackgroundColor: "#fff",
-                pointBorderWidth: 5,
-                pointHoverRadius: 5,
-                pointHoverBorderColor: "#fff",
-                pointHoverBorderWidth: 1,
-                pointRadius: 0,
-                pointHitRadius: 1,
-                data: [20, 14, 21, 15, 22, 8, 18, 13, 21, 13, 17, 13, 20, 15],
-                spanGaps: false
-            }]
-        }
-    });
+    //var LINECHART = $('#lineChart2');
+    //var myLineChart = new Chart(LINECHART, {
+    //    type: 'line',
+    //    options: {
+    //        scales: {
+    //            xAxes: [{
+    //                display: false
+    //            }],
+    //            yAxes: [{
+    //                ticks: {
+    //                    max: 50,
+    //                    min: 0
+    //                },
+    //                display: false
+    //            }]
+    //        },
+    //        legend: {
+    //            display: false
+    //        }
+    //    },
+    //    data: {
+    //        labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"],
+    //        datasets: [{
+    //            label: "Page Visitors",
+    //            fill: true,
+    //            lineTension: 0.4,
+    //            backgroundColor: "transparent",
+    //            borderColor: blue,
+    //            pointBorderColor: blue,
+    //            pointHoverBackgroundColor: blue,
+    //            borderCapStyle: 'butt',
+    //            borderDash: [],
+    //            borderDashOffset: 0.0,
+    //            borderJoinStyle: 'miter',
+    //            borderWidth: 3,
+    //            pointBackgroundColor: "#fff",
+    //            pointBorderWidth: 5,
+    //            pointHoverRadius: 5,
+    //            pointHoverBorderColor: "#fff",
+    //            pointHoverBorderWidth: 1,
+    //            pointRadius: 0,
+    //            pointHitRadius: 1,
+    //            data: [20, 14, 21, 15, 22, 8, 18, 13, 21, 13, 17, 13, 20, 15],
+    //            spanGaps: false
+    //        }]
+    //    }
+    //});
 
 
-    // ------------------------------------------------------- //
-    // Pie Chart
-    // ------------------------------------------------------ //
-    var PIECHART = $('#pieChartHome1');
-    var myPieChart = new Chart(PIECHART, {
-        type: 'doughnut',
-        options: {
-            cutoutPercentage: 90,
-            legend: {
-                display: false
-            }
-        },
-        data: {
-            labels: [
-                "First",
-                "Second",
-                "Third"
-            ],
-            datasets: [{
-                data: [250, 200],
-                borderWidth: [0, 0],
-                backgroundColor: [
-                    green,
-                    "#eee",
-                ],
-                hoverBackgroundColor: [
-                    green,
-                    "#eee",
-                ]
-            }]
-        }
-    });
+    //// ------------------------------------------------------- //
+    //// Line Chart 3
+    //// ------------------------------------------------------ //
+
+    //var LINECHART = $('#lineChart3');
+    //var myLineChart = new Chart(LINECHART, {
+    //    type: 'line',
+    //    options: {
+    //        scales: {
+    //            xAxes: [{
+    //                display: false
+    //            }],
+    //            yAxes: [{
+    //                ticks: {
+    //                    max: 50,
+    //                    min: 0
+    //                },
+    //                display: false
+    //            }]
+    //        },
+    //        legend: {
+    //            display: false
+    //        }
+    //    },
+    //    data: {
+    //        labels: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"],
+    //        datasets: [{
+    //            label: "Page Visitors",
+    //            fill: true,
+    //            lineTension: 0.4,
+    //            backgroundColor: "transparent",
+    //            borderColor: red,
+    //            pointBorderColor: red,
+    //            pointHoverBackgroundColor: red,
+    //            borderCapStyle: 'butt',
+    //            borderDash: [],
+    //            borderDashOffset: 0.0,
+    //            borderJoinStyle: 'miter',
+    //            borderWidth: 3,
+    //            pointBackgroundColor: "#fff",
+    //            pointBorderWidth: 5,
+    //            pointHoverRadius: 5,
+    //            pointHoverBorderColor: "#fff",
+    //            pointHoverBorderWidth: 1,
+    //            pointRadius: 0,
+    //            pointHitRadius: 1,
+    //            data: [20, 14, 21, 15, 22, 8, 18, 13, 21, 13, 17, 13, 20, 15],
+    //            spanGaps: false
+    //        }]
+    //    }
+    //});
 
 
-    // ------------------------------------------------------- //
-    // Pie Chart
-    // ------------------------------------------------------ //
-    var PIECHART = $('#pieChartHome2');
-    var myPieChart = new Chart(PIECHART, {
-        type: 'doughnut',
-        options: {
-            cutoutPercentage: 90,
-            legend: {
-                display: false
-            }
-        },
-        data: {
-            labels: [
-                "First",
-                "Second"
-            ],
-            datasets: [{
-                data: [300, 50],
-                borderWidth: [0, 0],
-                backgroundColor: [
-                    blue,
-                    "#eee"
-                ],
-                hoverBackgroundColor: [
-                    blue,
-                    "#eee"
-                ]
-            }]
-        }
-    });
+    //// ------------------------------------------------------- //
+    //// Pie Chart
+    //// ------------------------------------------------------ //
+    //var PIECHART = $('#pieChartHome1');
+    //var myPieChart = new Chart(PIECHART, {
+    //    type: 'doughnut',
+    //    options: {
+    //        cutoutPercentage: 90,
+    //        legend: {
+    //            display: false
+    //        }
+    //    },
+    //    data: {
+    //        labels: [
+    //            "First",
+    //            "Second",
+    //            "Third"
+    //        ],
+    //        datasets: [{
+    //            data: [250, 200],
+    //            borderWidth: [0, 0],
+    //            backgroundColor: [
+    //                green,
+    //                "#eee",
+    //            ],
+    //            hoverBackgroundColor: [
+    //                green,
+    //                "#eee",
+    //            ]
+    //        }]
+    //    }
+    //});
 
 
-    // ------------------------------------------------------- //
-    // Pie Chart
-    // ------------------------------------------------------ //
-    var PIECHART = $('#pieChartHome3');
-    var myPieChart = new Chart(PIECHART, {
-        type: 'doughnut',
-        options: {
-            cutoutPercentage: 90,
-            legend: {
-                display: false
-            }
-        },
-        data: {
-            labels: [
-                "First",
-                "Second"
-            ],
-            datasets: [{
-                data: [300, 50],
-                borderWidth: [0, 0],
-                backgroundColor: [
-                    violet,
-                    "#eee"
-                ],
-                hoverBackgroundColor: [
-                    violet,
-                    "#eee"
-                ]
-            }]
-        }
-    });
+    //// ------------------------------------------------------- //
+    //// Pie Chart
+    //// ------------------------------------------------------ //
+    //var PIECHART = $('#pieChartHome2');
+    //var myPieChart = new Chart(PIECHART, {
+    //    type: 'doughnut',
+    //    options: {
+    //        cutoutPercentage: 90,
+    //        legend: {
+    //            display: false
+    //        }
+    //    },
+    //    data: {
+    //        labels: [
+    //            "First",
+    //            "Second"
+    //        ],
+    //        datasets: [{
+    //            data: [300, 50],
+    //            borderWidth: [0, 0],
+    //            backgroundColor: [
+    //                blue,
+    //                "#eee"
+    //            ],
+    //            hoverBackgroundColor: [
+    //                blue,
+    //                "#eee"
+    //            ]
+    //        }]
+    //    }
+    //});
 
 
-    // ------------------------------------------------------- //
-    // Pie Chart
-    // ------------------------------------------------------ //
-    var PIECHART = $('#pieChartHome4');
-    var myPieChart = new Chart(PIECHART, {
-        type: 'doughnut',
-        options: {
-            cutoutPercentage: 90,
-            legend: {
-                display: false
-            }
-        },
-        data: {
-            labels: [
-                "First",
-                "Second"
-            ],
-            datasets: [{
-                data: [200, 80],
-                borderWidth: [0, 0],
-                backgroundColor: [
-                    green,
-                    "#eee"
-                ],
-                hoverBackgroundColor: [
-                    green,
-                    "#eee"
-                ]
-            }]
-        }
-    });
+    //// ------------------------------------------------------- //
+    //// Pie Chart
+    //// ------------------------------------------------------ //
+    //var PIECHART = $('#pieChartHome3');
+    //var myPieChart = new Chart(PIECHART, {
+    //    type: 'doughnut',
+    //    options: {
+    //        cutoutPercentage: 90,
+    //        legend: {
+    //            display: false
+    //        }
+    //    },
+    //    data: {
+    //        labels: [
+    //            "First",
+    //            "Second"
+    //        ],
+    //        datasets: [{
+    //            data: [300, 50],
+    //            borderWidth: [0, 0],
+    //            backgroundColor: [
+    //                violet,
+    //                "#eee"
+    //            ],
+    //            hoverBackgroundColor: [
+    //                violet,
+    //                "#eee"
+    //            ]
+    //        }]
+    //    }
+    //});
+
+
+    //// ------------------------------------------------------- //
+    //// Pie Chart
+    //// ------------------------------------------------------ //
+    //var PIECHART = $('#pieChartHome4');
+    //var myPieChart = new Chart(PIECHART, {
+    //    type: 'doughnut',
+    //    options: {
+    //        cutoutPercentage: 90,
+    //        legend: {
+    //            display: false
+    //        }
+    //    },
+    //    data: {
+    //        labels: [
+    //            "First",
+    //            "Second"
+    //        ],
+    //        datasets: [{
+    //            data: [200, 80],
+    //            borderWidth: [0, 0],
+    //            backgroundColor: [
+    //                green,
+    //                "#eee"
+    //            ],
+    //            hoverBackgroundColor: [
+    //                green,
+    //                "#eee"
+    //            ]
+    //        }]
+    //    }
+    //});
 
 
 });
