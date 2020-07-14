057b4379e3cf9c312ef8ded27b6b343cbf74a22f
HieuVV <voxvanhieu@gmail.com>
2020-07-15 01:33:23 +0700

    Do many things - Chang EF identity model: Address -> Contact - Upload xlsx file - Import views - Admin index

--- src/OnlineHelpDesk/Controllers/AdminController.cs
+++ src/OnlineHelpDesk/Controllers/AdminController.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using OnlineHelpDesk.Models;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Razor.Generator;
 
 namespace OnlineHelpDesk.Controllers
 {
@@ -13,9 +17,56 @@ namespace OnlineHelpDesk.Controllers
         public ActionResult Index()
         {
             return View();
+        } 
+
+        public ActionResult Import(string importFor)
+        {
+            ViewBag.ImportFor = importFor;
+            return View();
         }
+
+        //[HttpPost]
+        //public async Task<ActionResult> ImportStudents()
+        //{
+        //    return View();
+        //}
 
         // IMPORT DATA FROM EXCEL
         // MANUAL GENERATE REPORT
+
+        [HttpPost]
+        public ActionResult UploadExcel(ImportDataiewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //using (MemoryStream memoryStream = new MemoryStream())
+                //{
+                //    model.File.InputStream.CopyTo(memoryStream);
+                //}
+
+                try
+                {
+                    var file = model.File;
+                    if (file.ContentLength > 0)
+                    {
+                        string filename = Path.GetFileName(file.FileName);
+                        string path = Path.Combine(Server.MapPath("~/Content/Uploaded"), filename);
+
+                        file.SaveAs(path);
+                    }
+
+                    //ViewBag.Message = "File upload successfully!";
+                    return View("Index");
+                }
+                catch (Exception e)
+                {
+
+                    ViewBag.Message = e.Message;
+                    return View("Index");
+                }
+            }
+
+            return View("Index");
+        }
     }
 }
---
+++ src/OnlineHelpDesk/Models/ViewModels/AdminViewModels.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace OnlineHelpDesk.Models
+{
+    public class ImportDataiewModel
+    {
+        [Required(ErrorMessage ="Please upload a CSV file")]
+        public HttpPostedFileBase File { get; set; }
+    }
+}
